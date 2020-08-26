a883dc4a4ee08d1cbbf3487b4d0d83f0ff4598d9
arutselvann <58092288+arutselvann@users.noreply.github.com>
2020-08-26 16:17:39 -0700

    Add files via upload

---
+++ college income analysis.R
@@ -0,0 +1,8 @@
+install.packages("devtools")
+devtools::install_github("jhudsl/collegeIncome")
+library(collegeIncome)
+data(college)
+devtools::install_github("jhudsl/matahari")
+library(matahari)
+dance_start(value = FALSE, contents = FALSE)
+dance_save("~/Desktop/college_major_analysis.rds")
